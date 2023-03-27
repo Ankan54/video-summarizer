5b93aca550203c2df230e4e05c68a78798ff38c4
ankan.bera <eklapothik54@gmail.com>
2023-03-27 16:34:16 +0530

    initial commit

---
+++ audio_transcription.py
@@ -0,0 +1,24 @@
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+import openai
+openai.api_key = "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"
+
+def transcribe_audio():
+    audio_file= open(r".\media\temp_audio.mp4", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript['text']
+
+
+def summarise_recording():
+    text= transcribe_audio()
+    with open(r".\media\temp_transcript.txt","w") as txt_file:
+        txt_file.write(text) 
+    prompt= f"Summarize the following text in a numerical point wise manner:\n{text}"
+    completion = openai.Completion.create(model="text-davinci-003",
+                                        prompt=prompt,
+                                        temperature=0.7,
+                                        max_tokens= 500,
+                                        top_p= 1.0,
+                                        frequency_penalty= 0.0,
+                                        presence_penalty=1)
+
+    return completion.choices[0].text.strip("\n")
---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify, render_template
+from audio_transcription import summarise_recording
+app = Flask(__name__)
+
+recording_summary= ""
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/transcribe', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        file.save(r".\media\temp_audio.mp4")
+        recording_summary = summarise_recording()
+        return jsonify({"summary": recording_summary})
+    else:
+        error = 'Some Error Occured!'
+        return jsonify({'error': error})
+
+
+@app.route('/chatbot')
+def chatbot():
+    message = request.args.get('message', '')
+    # Handle chatbot logic here
+    response = "Hello! How can I help you?"
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost',port='5100')
